e376a1b59b84f9aeccdbc4bd6503df97ffe06125
navinelahi <tnavin191056@bscse.uiu.ac.bd>
2023-12-23 12:15:36 +0600

    commit latest

--- myproject_original/authorize/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from authorize.models import *
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password')
--- myproject_original/business/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis.db import models
-
-class Business(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=300)
-    coordinates = models.PointField()
-
-    def __str__(self):
-        return self.name
